c948ec18dd337f240d5faf2fbab29dc7b8dcba43
Nicolas CLAIN <nicolas.clain@cr-reunion.fr>
2016-11-23 22:14:58 +0400

    init

---
+++ nuxeo-operations-core/src/main/java/nuxeo/operations/AddComment.java
@@ -0,0 +1,34 @@
+package nuxeo.operations;
+
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.platform.comment.api.CommentManager;
+import org.nuxeo.runtime.api.Framework;
+
+/**
+ *
+ */
+@Operation(id=AddComment.ID, category=Constants.CAT_DOCUMENT, label="Ajout d'un commentaire sur un document", description="Describe here what your operation does.")
+public class AddComment {
+
+    public static final String ID = "Document.AddComment";
+
+    @Context
+    protected CoreSession session;
+
+    @Param(name = "comment", required = true)
+    protected String comment;
+
+    @OperationMethod
+    public void run(DocumentModel doc) {
+    	CommentManager commentManager = Framework.getService(CommentManager.class);
+    	NuxeoPrincipal user = (NuxeoPrincipal) session.getPrincipal();
+    	commentManager.createComment(doc, comment, user.getName());
+    }
+}
